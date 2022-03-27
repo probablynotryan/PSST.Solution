3375d28044682c9eca7d4d3dfd5ad4743ce41bce
Ryan Bass <theryanbass@gmail.com>
2022-03-27 12:50:39 -0700

    add security dependencies with specified view

--- PSST/Controllers/HomeController.cs
+++ PSST/Controllers/HomeController.cs
@@ -1,21 +1,38 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using System.Linq;
 using Treats.Models;
+using Treats.ViewModels;
 
 namespace Treats.Controllers
 {
   public class HomeController : Controller
   {
     private readonly TreatsContext _db;
+
     public HomeController(TreatsContext db)
     {
       _db = db;
     }
 
-        [HttpGet("/")]
+    [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+      var HomeModel = new UserViewModel {
+        UserId = userId,
+        Flavors = _db.Flavors.ToList()
+          .OrderByDescending(flavor => flavor.User.Id == userId)
+          .ToList(),
+        Sweets = _db.Sweets
+          .OrderByDescending(sweet => sweet.User.Id == userId)
+          .ToList()
+      };
+
+      return View(HomeModel);
     }
   }
 }
